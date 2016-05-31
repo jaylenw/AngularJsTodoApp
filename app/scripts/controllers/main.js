795413fcda040cbd2bd2bb397b3acffd6fb82cbc
jaylenw <mr2cleanj@live.com>
2016-05-30 22:51:55 -0700

    basic layout set

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,33 @@
  * # MainCtrl
  * Controller of the todoApp
  */
+//Initializing AngularJS app by creating a module, app name is 'todoApp'
+
+ //Creating a controller named 'MainCtrl' and adding it to our app
+ //adding '$scope' into the function so that our view has access to the controller's
+ //data. This is called dependency injection
+
 angular.module('todoApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', function ($scope) {
+
+    //making an object of todos notes in the object of todos list
+      $scope.todoList = [
+        {
+          title: "Note A",
+          text: "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ",
+          taskCompleted: true
+        },
+        {
+          title: "Note B",
+          text: "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ",
+          taskCompleted: false
+        },
+        {
+          title: "Note C",
+          text: "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ",
+          taskCompleted: false
+        }];
+
+
+
+  });//end of controller
