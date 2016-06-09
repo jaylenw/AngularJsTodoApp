6eb8973f2433681a5a30a5958a8d078fa4499aef
jaylenw <mr2cleanj@live.com>
2016-06-09 00:27:12 -0700

    user notifications are working for user login

--- app/scripts/controllers/auth.js
+++ app/scripts/controllers/auth.js
@@ -10,44 +10,73 @@
 angular.module('todoApp')
   .controller('authCtrl', function ($scope, $window, User, ngNotify) {
 
+    //Initializing our user object
     $scope.user = {};
 
+    //registering the user
     $scope.register = function() {
       User.register($scope.user, function(response) {
-        ngNotify.set('You are now Registered.', 'success');
+        ngNotify.set('You are now Registered. Redirecting...', 'success');
         console.log(response);
         //storing token from server into browser
         localStorage.setItem("token", response.token);
         //redirecting user to home page where task lies after 4000ms
         setInterval(function(){
           $window.location.href = '/';
-        }, 4000);
+        }, 4000);//4000ms because notification timeout is 4000ms
       }, function(err) {
 
-        switch(err.status){
-          case 406:
-            ngNotify.set('Email address is not valid. Please enter a valid Email address.', 'error');
-            break;
-          case 409:
-            ngNotify.set('Email has already been registerd. Please enter another Email address.', 'error');
-            break;
-          case 412:
-            ngNotify.set('Email address and Password were not entered successfully. Please enter them again.', 'error');
-            break;
-          case 500:
-            ngNotify.set('We could not save your account. Please try again.', 'error');
-            break;
-          default:
-            ngNotify.set('An error occured processing your request to the server. Please try again.', 'error');
-        }
+          switch(err.status){
+            case 406:
+              ngNotify.set(' Entered Email address is not valid. Please enter a valid Email address.', 'error');
+              break;
+            case 409:
+              ngNotify.set('Entered Email has already been registerd. Please enter another Email address.', 'error');
+              break;
+            case 412:
+              ngNotify.set('Entered Email address and Password were not entered successfully. Please enter them again.', 'error');
+              break;
+            case 500:
+              ngNotify.set('We could not save your account. Please try again.', 'error');
+              break;
+            default:
+              ngNotify.set('An error occured processing your request to the server. Please try again.', 'error');
+          }
         console.log(err);
       });
 
     };
 
-    $scope.login = function(){
+  //allowing the user to login
+  $scope.login = function(){
+    User.login($scope.user, function(response){
+      ngNotify.set('You are now Logged in. Redirecting...', 'success');
+      console.log(response);
+      //storing token from server into browser
+      localStorage.setItem("token", response.token);
+      //redirecting user to home page where task lies after 4000ms
+      setInterval(function(){
+        $window.location.href = '/';
+      }, 4000);//4000ms because notification timeout is 4000ms
 
-    };
+    }, function(err){
+
+        switch (err.status) {
+          case 401:
+            ngNotify.set('Entered Email address or Password are incorrect. Please try again.', 'error');
+            break;
+          case 412:
+            ngNotify.set('Please enter an email address and password and try again.', 'error');
+            break;
+          case 500:
+            ngNotify.set('Could not find your account. Please enter your credentials again.', 'error');
+            break;
+          default:
+            ngNotify.set('An error occured processing your request to the server. Please try again.', 'error');
+        }
+        console.log(err);
+    });
+  };
 
 
 
