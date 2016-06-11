056277ef24a0ca2bba21b0b913a2a03a9bbb11c0
jaylenw <mr2cleanj@live.com>
2016-06-11 01:22:14 -0700

    removed console.logs

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -24,15 +24,12 @@ angular.module('todoApp')
       $window.location.href = '#/auth';
     }
 
-
     //Initializing variable to false for showing archive notes
     $scope.showArchive = false;
 
     //making an object of todos notes in the object of todos list
     $scope.todoList = Task.get({"token":$scope.userToken}, function(response){
-      console.log("got tasks");
     }, function(err){
-      console.log("did not obtain tasks");
     });
     //Initializing a blank note for ng-model in jumbotron of index.html
     $scope.note = {
@@ -44,10 +41,8 @@ angular.module('todoApp')
     //add note to todoList
     $scope.addNote = function(noteTitle, noteBody){
       var serverNode = Task.create({"title": noteTitle,"body":noteBody ,"token":$scope.userToken}, function(response){
-      console.log("note added successfully ");
       ngNotify.set("Syncing Note with the Server.",'success');
       }, function(err){
-          console.log("error while adding note" + err.status );
           ngNotify.set('Error, Note could not be added. Try again later.','error');
       });
       //push temp note to todoList
@@ -69,7 +64,6 @@ angular.module('todoApp')
 
     //update (edit) note archive status
     $scope.updateNoteArchive = function(noteID, noteArchive){
-      console.log(noteID, noteArchive);
       var payload = {id:noteID, "archive":noteArchive, "token":$scope.userToken};
       Task.update(payload ,function(success){
         ngNotify.set('Note updated Successfully', 'success');
