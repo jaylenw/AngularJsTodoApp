bb6b76e5475e5af179e8f796f640b473f8b297a1
Jaylen Wimbish <jaylenw@users.noreply.github.com>
2016-06-30 23:25:59 -0700

    Update task.js

--- app/scripts/services/task.js
+++ app/scripts/services/task.js
@@ -1,6 +1,6 @@
 angular.module('todoApp')
 .factory("Task",['$resource', function($resource){
-  return $resource("http://yourdomain:3000/tasks", {},{
+  return $resource("http://yourdomain.com:3000/tasks", {},{
     get:{
       method:"GET",
       params:{},
@@ -15,7 +15,7 @@ angular.module('todoApp')
       method:"PUT",
       params:{ id: '@id' },
       isArray: false,
-      url: "http://yourdomain:3000/tasks/:id"
+      url: "http://yourdomain.com:3000/tasks/:id"
     }
   })
 }]);
