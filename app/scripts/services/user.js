f6279b2a34944aaaa4124cf89893def7c29c8eab
Jaylen Wimbish <jaylenw@users.noreply.github.com>
2016-06-30 21:46:49 -0700

    change to yourdomain.com

--- app/scripts/services/user.js
+++ app/scripts/services/user.js
@@ -1,17 +1,17 @@
 angular.module('todoApp')
 .factory("User",['$resource', function($resource){
-  return $resource("http://todo.jaylenwimbish.com:3000/users", {},{
+  return $resource("http://yourdomain.com:3000/users", {},{
     login:{
       method:"POST",
       params:{},
       isArray: false,
-      url: "http://todo.jaylenwimbish.com:3000/users/login"
+      url: "http://yourdomain:3000/users/login"
     },
     register:{
       method:"POST",
       params:{},
       isArray: false,
-      url: "http://todo.jaylenwimbish.com:3000/users/register"
+      url: "http://yourdomain:3000/users/register"
     }
   })
 }]);
