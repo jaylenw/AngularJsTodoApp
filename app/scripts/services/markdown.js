86095a1a7608c0f46fbbf8c1a2205bd1b8ea45f3
jaylenw <mr2cleanj@live.com>
2016-12-30 11:27:02 +0000

    changes added for markdown rendering

---
+++ app/scripts/services/markdown.js
@@ -0,0 +1,16 @@
+angular.module('todoApp').directive('markdown', function ($sanitize) {
+    var converter = new showdown.Converter();
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            function renderMarkdown() {
+                var htmlText = converter.makeHtml(scope.$eval(attrs.markdown)  || '');
+                element.html($sanitize(htmlText));
+            }
+            scope.$watch(attrs.markdown, function(){
+                renderMarkdown();
+            });
+            renderMarkdown();
+        }
+    }
+});
