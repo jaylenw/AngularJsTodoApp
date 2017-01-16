3ea42cf7e719cfffd8feb7cba4180311d4e028a2
jaylenw <mr2cleanj@live.com>
2017-01-16 01:54:28 -0800

    added jasmine tests for MainCtrl

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -17,7 +17,36 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
+  it('MainCtrl should attach a list of awesomeThings to the scope', function () {
     expect(MainCtrl.awesomeThings.length).toBe(3);
   });
+
+  it('token should be defined', function () {
+    expect(scope.token).toBeDefined();
+  });
+
+  it('note should be defined', function () {
+    expect(scope.note).toBeDefined();
+  });
+
+  it('addNote should be defined', function () {
+    expect(scope.addNote).toBeDefined();
+  });
+
+  it('updateNoteContent should be defined', function () {
+    expect(scope.updateNoteContent).toBeDefined();
+  });
+
+  it('updateNoteArchive should be defined', function () {
+    expect(scope.updateNoteArchive).toBeDefined();
+  });
+
+  it('deleteNote should be defined', function () {
+    expect(scope.deleteNote).toBeDefined();
+  });
+
+  it('formatDate should be defined', function () {
+    expect(scope.formatDate).toBeDefined();
+  });
+
 });
