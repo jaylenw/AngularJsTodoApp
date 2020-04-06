ab516aee59c91049238ea662c3bd16807b562500
jaylenw <mr2cleanj@live.com>
2020-04-05 20:25:43 -0700

    remove the dev api url
    close #66

--- app/scripts/services/config.js
+++ app/scripts/services/config.js
@@ -3,8 +3,7 @@
 angular.module('todoApp')
 .factory("Config",function(){
 
-  // var _apiUrl = "http://yourdomain.com:3000";
-  var _apiUrl = "https://api.nota-dev.jaylenwimbish.com";
+  var _apiUrl = "http://yourdomain.com:3000";
   var configService = {};
 
   configService.getAPI = function(){
